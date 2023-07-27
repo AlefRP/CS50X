d06b0bcb766c257957118c47aa2386590ecd3076
alefrp <alefrodrigopereira@yahoo.com.br>
2023-07-27 08:14:05 -0300

    feat: lectures update

---
+++ arrays/lecture/hi.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    /* char c1 = 'H';
+    char c2 = 'I'; 
+    char c3 = '!';
+
+    printf("%c%c%c\n", c1, c2, c3); */
+
+    string s = "HI!";
+    string t = "BYE!";
+    
+    // printf("%i %i %i %i", s[0], s[1], s[2], s[3]); // Printing each string's character
+    printf("%s\n", s);
+    printf("%s\n", t);
+}
---
+++ arrays/lecture/scores.c
@@ -0,0 +1,51 @@
+#include <cs50.h>
+#include <stdio.h>
+
+const int N = 3; //It is common to declare global variables at the beginning
+
+float average(int length, int array[]); // I need to declare a prototype 'cause I'll
+
+int main(void)
+{
+    // Bad Design
+    /* int score1 = 72;
+    int score2 = 73;
+    int score3 = 33; */
+
+    // Array
+    /* int scores[3]; // Declare an array that stores 3 scores
+    scores[0] = get_int("What's your score? ");
+    scores[1] = get_int("What's your score? ");
+    scores[2] = get_int("What's your score? "); */
+
+    int scores[N];
+    for (int i = 0; i < 3; i++)
+    {
+        scores[i] = get_int("What's your score? ");
+    }
+
+    // printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
+    //In C I need always to remenber the lenght of an array
+    printf("Average: %f\n", average(N, scores)); //casting typing
+}
+
+/* float average(int array[]) // How to make a function expects to an Array as Input
+{
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        sum += array[i]; //Or sum = sum + array[i]
+    }
+    return sum / (float) N;
+} */
+
+// I have puted two arguments in the function to make average indepedent
+float average(int length, int array[])
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        sum += array[i]; //Or sum = sum + array[i]
+    }
+    return sum / (float) length;
+}
