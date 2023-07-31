3e6a362d8acc7eae36f6c460678a615660e3b488
alefrp <alefrodrigopereira@yahoo.com.br>
2023-07-31 07:49:03 -0300

    update: arrays lecture

--- arrays/lecture/hi.c
+++ arrays/lecture/hi.c
@@ -9,10 +9,17 @@ int main(void)
 
     printf("%c%c%c\n", c1, c2, c3); */
 
-    string s = "HI!";
-    string t = "BYE!"; 
+    /* string s = "HI!";
+    string t = "BYE!";  */
+
+    string words[2];
+    words[0] = "HI";
+    words[1] = "BYE";
     
     // printf("%i %i %i %i", s[0], s[1], s[2], s[3]); // Printing each string's character
-    printf("%s\n", s);
-    printf("%s\n", t);
+    /* printf("%s\n", s);
+    printf("%s\n", t); */
+
+    printf("%c%c%c\n", words[0][0], words[0][1], words[0][2]);
+    printf("%c%c%c%c\n", words[1][0], words[1][1], words[1][2], words[1][3]);
 }
