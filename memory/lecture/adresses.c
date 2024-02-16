269d3d889be3f9fa35a765595eb974b0d6a241a8
alefrp <alefrodrigopereira@yahoo.com.br>
2024-02-15 21:27:19 -0300

    feat: memory lecture

---
+++ memory/lecture/adresses.c
@@ -0,0 +1,25 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    string s = "HI!";
+    // It was always char *s = "HI!";
+    printf("%s\n", s);
+    // printf("%p\n", s);
+    // printf("%p\n", &s[0]);
+    // printf("%p\n", &s[1]);
+    // printf("%p\n", &s[2]);
+    // printf("%p\n", &s[3]);
+}
+
+// int main(void)
+// {
+//     int n = 50;
+//     // int* p = &n; //same thing as down (int * p - or this)
+//     int *p = &n; // & is used to get the address - * is used to make a pointer, stores in p the adress of n
+//     // printf("%i\n", n);
+//     // printf("%p\n", &n); // %p is used to print the address - c suport pointers
+//     printf("%p\n", p);
+//     printf("%i\n", *p); // to see what is in memory - the adress for wich p is pointing
+// }
